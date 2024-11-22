2c0341cb055593e29e247dc10410e4f30c9de59a
Florian Lefebvre <contact@florian-lefebvre.dev>
2024-11-22 16:45:11 +0100

    feat: work on impl

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,70 +1,105 @@
-import type { AstroIntegration } from "astro";
+import type {
+  AstroConfig,
+  AstroIntegration,
+  AstroIntegrationLogger,
+} from "astro";
 import { name } from "../package.json";
 import { MutableDataStore } from "../node_modules/astro/dist/content/mutable-data-store.js";
 import { writeFileSync } from "node:fs";
-
-// TODO: handle non existing collections
-// TODO: handle collections param type when astro:content is not generated (see Erika tweet)
-// TODO: handle types during build and sync (different location)
 
 interface Options {
   collections: Array<import("astro:content").DataCollectionKey>;
 }
 
-export default function integration({
+const createUtils = ({
+  logger,
+  isDev,
   collections,
-}: Options): AstroIntegration {
+}: {
+  logger: AstroIntegrationLogger;
+  isDev: boolean;
+  collections: Array<string>;
+}) => {
   let dataStoreURL: URL;
-  let isDev: boolean;
   let store: MutableDataStore;
   let dtsURL: URL;
 
-  const loadStore = async () => {
-    store = await MutableDataStore.fromFile(dataStoreURL);
-  };
+  return {
+    init: async ({ config, dts }: { config: AstroConfig; dts: URL }) => {
+      dataStoreURL = isDev
+        ? new URL("./.astro/data-store.json", config.root)
+        : new URL("./data-store.json", config.cacheDir);
+      dtsURL = dts;
+    },
+    updateStore: async () => {
+      store = await MutableDataStore.fromFile(dataStoreURL);
+    },
+    shouldSync: (path: string) => {
+      return path === dataStoreURL.pathname;
+    },
+    sync: () => {
+      const obj: Record<string, Array<string>> = {};
 
-  const getDtsContent = (): string => {
-    const obj: Record<string, Array<string>> = {};
+      for (const id of collections) {
+        const collection = store.collections().get(id);
+        if (collection) {
+          const keys = Array.from(collection.keys());
+          obj[id] = keys;
+          if (keys.length > 200) {
+            logger.warn(
+              `Collection "${id}" has more than 200 entries. The integration is supposed to be used for manual usages, it could affect performance`
+            );
+          }
+        } else {
+          logger.warn(`Collection "${id}" does not exist`);
+        }
+      }
 
-    for (const id of collections) {
-      obj[id] = [...(store.collections().get(id)?.keys() ?? [])];
-    }
-
-    return `declare module 'astro:content' {
+      const content = `declare module 'astro:content' {
   type _Collections = ${JSON.stringify(obj)};
   export type CollectionId<T extends keyof _Collections> = _Collections[T][number];
 }`;
-  };
 
-  const syncTypes = () => {
-    writeFileSync(dtsURL, getDtsContent(), "utf-8");
+      writeFileSync(dtsURL, content, "utf-8");
+    },
   };
+};
+
+type Utils = ReturnType<typeof createUtils>;
+
+export default function integration({
+  collections,
+}: Options): AstroIntegration {
+  let utils: Utils;
 
   return {
     name,
     hooks: {
-      "astro:config:setup": (params) => {
-        isDev = params.command === "dev";
-      },
-      "astro:config:done": async (params) => {
-        dataStoreURL = isDev
-          ? new URL("./.astro/data-store.json", params.config.root)
-          : new URL("./data-store.json", params.config.cacheDir);
-        await loadStore();
-        // In sync, it will always have an outdated type because the data store is updated
-        // later than this hook
-        dtsURL = params.injectTypes({
-          filename: "types.d.ts",
-          content: getDtsContent(),
+      "astro:config:setup": ({ command, logger }) => {
+        utils = createUtils({
+          isDev: command === "dev",
+          logger,
+          collections,
         });
       },
+      "astro:config:done": async (params) => {
+        await utils.init({
+          config: params.config,
+          // In sync, it will always have an outdated type because the data store is updated
+          // later than this hook
+          dts: params.injectTypes({
+            filename: "types.d.ts",
+            content: "",
+          }),
+        });
+        await utils.updateStore();
+      },
       "astro:server:setup": ({ server }) => {
-        syncTypes();
-        server.watcher.on("all", async (eventName, path) => {
-          if (!["change"].includes(eventName)) return;
-          if (path === dataStoreURL.pathname) {
-            await loadStore();
-            syncTypes();
+        utils.sync();
+        server.watcher.on("change", async (path) => {
+          if (utils.shouldSync(path)) {
+            await utils.updateStore();
+            utils.sync();
           }
         });
       },
